8b1be24cdeeb62e92194f5880ec2564e438c5c49
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-09-30 00:15:38 -0400

    Updated Readme

---
+++ Assignment1/README.md
@@ -0,0 +1,5 @@
+In order to run this project please run script.py with python 3.x installation.
+
+Please have internet connection before running project for successful cloning of repositories. 
+
+I have also included a VR_Project_List_short.txt file to test without having to clone all 100+ repositories.
--- Assignment1/deleteRepos.py
+++ Assignment1/deleteRepos.py
@@ -1,13 +1,13 @@
-import shutil
-import os
+# import shutil
+# import os
 
-folder_path = ".\\Repos\\"
+# folder_path = ".\\Repos\\"
 
-for filename in os.listdir(folder_path):
-   file_path = os.path.join(folder_path, filename)
-   if os.path.isfile(file_path) or os.path.islink(file_path):
-      os.unlink(file_path)
-      print(filename, "is removed")
-   elif os.path.isdir(file_path):
-      shutil.rmtree(file_path)
-      print(filename, "subdirectory is removed")
+# for filename in os.listdir(folder_path):
+#    file_path = os.path.join(folder_path, filename)
+#    if os.path.isfile(file_path) or os.path.islink(file_path):
+#       os.unlink(file_path)
+#       print(filename, "is removed")
+#    elif os.path.isdir(file_path):
+#       shutil.rmtree(file_path)
+#       print(filename, "subdirectory is removed")
