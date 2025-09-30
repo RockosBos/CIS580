bd6718a75fbb56ba0a1320f47088bb99b8688806
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-09-29 23:35:33 -0400

    Commit processing and CSV storage

--- Assignment1/script.py
+++ Assignment1/script.py
@@ -1,10 +1,16 @@
+import os
+import csv
+
 from git import Repo
+from dataclasses import dataclass
 
 print("Would you like to Clone Repos (Y/N)?")
 cloneRepoInput = input()
 
-inputFile = open("./Input/VR_Project_List_short.txt")
+inputFile = open("./Input/VR_Project_List.txt")
 lines = inputFile.readlines()
+
+commitList = []
 
 if(cloneRepoInput == "Y" or cloneRepoInput == "y"):
 
@@ -22,14 +28,32 @@ if(cloneRepoInput == "Y" or cloneRepoInput == "y"):
 
 print("Analyzing Repositories")
 
-for line in lines:
-    line = line.replace("\n", "")
-    currentRepoURL = line
-    currentRepoPath = f"./Repos/{currentRepoURL.split('.git')[0].split('/')[-1]}"
+for folder in next(os.walk('./Repos/'))[1]:
+    print(f"Reading {folder}...")
+    repo  = Repo(f"./Repos/{folder}")
+    all_commits = list(repo.iter_commits(repo.active_branch))
+    for commit in all_commits:
 
-    print(currentRepoPath)
-    try:
-        currentRepoLocal = Repo(f"{currentRepoPath}/")
-        print(currentRepoLocal.commit('master'))
-    except:
-        print(f"Failed to open repo")
+        print(f"Reading Commit {commit} in repo {folder}")
+
+        if ("performance" in commit.message.lower() or
+            "speed up" in commit.message.lower() or
+            "accelerate" in commit.message.lower() or
+            "fast" in commit.message.lower() or
+            "slow" in commit.message.lower() or
+            "latency" in commit.message.lower() or
+            "contention" in commit.message.lower() or
+            "optimize" in commit.message.lower() or
+            "efficiency" in commit.message.lower()
+            ):
+
+            commitData = {"Repo":folder, "Message":commit.message, "Commit":commit}
+            commitList.append(commitData)
+
+with open('./Output/Result.csv', 'w') as f:
+    fieldNames = ["Repo", "Message", "Commit"]
+    writer = csv.DictWriter(f, fieldnames=fieldNames)
+    writer.writeheader()
+    writer.writerows(commitList)
+
+
