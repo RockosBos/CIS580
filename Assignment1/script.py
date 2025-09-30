33928425acbd65f20aef6f50fe802d984b3dfaf9
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-09-30 00:11:40 -0400

    Additional Error Handling

--- Assignment1/script.py
+++ Assignment1/script.py
@@ -4,6 +4,7 @@ import csv
 from git import Repo
 from dataclasses import dataclass
 
+
 print("Would you like to Clone Repos (Y/N)?")
 cloneRepoInput = input()
 
@@ -29,31 +30,39 @@ if(cloneRepoInput == "Y" or cloneRepoInput == "y"):
 print("Analyzing Repositories")
 
 for folder in next(os.walk('./Repos/'))[1]:
-    print(f"Reading {folder}...")
-    repo  = Repo(f"./Repos/{folder}")
-    all_commits = list(repo.iter_commits(repo.active_branch))
-    for commit in all_commits:
+    
+	try:
+		print(f"Reading {folder}...")
+		repo  = Repo(f"./Repos/{folder}")
+		all_commits = list(repo.iter_commits(repo.active_branch))
+		for commit in all_commits:
 
-        print(f"Reading Commit {commit} in repo {folder}")
+			print(f"Reading Commit {commit} in repo {folder}")
 
-        if ("performance" in commit.message.lower() or
-            "speed up" in commit.message.lower() or
-            "accelerate" in commit.message.lower() or
-            "fast" in commit.message.lower() or
-            "slow" in commit.message.lower() or
-            "latency" in commit.message.lower() or
-            "contention" in commit.message.lower() or
-            "optimize" in commit.message.lower() or
-            "efficiency" in commit.message.lower()
-            ):
+			if ("performance" in commit.message.lower() or
+				"speed up" in commit.message.lower() or
+				"accelerate" in commit.message.lower() or
+				"fast" in commit.message.lower() or
+				"slow" in commit.message.lower() or
+				"latency" in commit.message.lower() or
+				"contention" in commit.message.lower() or
+				"optimize" in commit.message.lower() or
+				"efficiency" in commit.message.lower()
+				):
 
-            commitData = {"Repo":folder, "Message":commit.message, "Commit":commit}
-            commitList.append(commitData)
+				commitData = {"Repo":folder, "Message":commit.message, "Commit":commit}
+				commitList.append(commitData)
+	except:
+		print(f"Failure reading commit data from {folder}. Commit ID {commit}")
+        
 
-with open('./Output/Result.csv', 'w') as f:
-    fieldNames = ["Repo", "Message", "Commit"]
-    writer = csv.DictWriter(f, fieldnames=fieldNames)
-    writer.writeheader()
-    writer.writerows(commitList)
+try:
+	with open('./Output/Result.csv', 'w', encoding='utf-8') as f:
+		fieldNames = ["Repo", "Message", "Commit"]
+		writer = csv.DictWriter(f, fieldnames=fieldNames)
+		writer.writeheader()
+		writer.writerows(commitList)
+except Exception as e:
+	print(f"Failure to write to csv. Reason {e}")
 
 
